b99d8a0f8d3ce8822e749db89bf9a5cb87d883f9
luna <iszychen@gmail.com>
2020-07-16 22:19:57 +0800

    project restart

--- luna-commons-media/src/main/java/com/luna/media/config/FfmpegConfigValue.java
+++ luna-commons-media/src/main/java/com/luna/media/config/FfmpegConfigValue.java
@@ -1,4 +1,4 @@
-package com.luna.media.ffmpeg;
+package com.luna.media.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
