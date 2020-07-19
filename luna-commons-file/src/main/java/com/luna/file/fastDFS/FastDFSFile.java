2c34c31fe088645e5927f91d35ff12059a0be74e
luna <iszychen@gmail.com>
2020-07-19 11:17:38 +0800

    fastdfs文件处理

---
+++ luna-commons-file/src/main/java/com/luna/file/fastDFS/FastDFSFile.java
@@ -0,0 +1,82 @@
+package com.luna.file.fastDFS;
+
+import java.io.Serializable;
+
+/**
+ * 描述
+ *
+ * @author 三国的包子
+ * @version 1.0
+ * @package com.changgou.file *
+ * @since 1.0
+ */
+public class FastDFSFile implements Serializable {
+
+    //文件名字
+    private String name;
+    //文件内容
+    private byte[] content;
+    //文件扩展名
+    private String ext;
+    //文件MD5摘要值
+    private String md5;
+    //文件创建作者
+    private String author;
+
+    public FastDFSFile(String name, byte[] content, String ext, String md5, String author) {
+        this.name = name;
+        this.content = content;
+        this.ext = ext;
+        this.md5 = md5;
+        this.author = author;
+    }
+
+    public FastDFSFile(String name, byte[] content, String ext) {
+        this.name = name;
+        this.content = content;
+        this.ext = ext;
+    }
+
+    public FastDFSFile() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public String getExt() {
+        return ext;
+    }
+
+    public void setExt(String ext) {
+        this.ext = ext;
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
